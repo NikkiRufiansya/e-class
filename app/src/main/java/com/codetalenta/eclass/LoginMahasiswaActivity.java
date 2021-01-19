92668987cf31b4c4f6e5cf9753b6d47707c89ca3
noverio24 <noverio99@gmail.com>
2021-01-19 17:28:01 +0800

    layout homepage

---
+++ app/src/main/java/com/codetalenta/eclass/LoginMahasiswaActivity.java
@@ -0,0 +1,16 @@
+package com.codetalenta.eclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codetalenta.eclass.R;
+
+public class LoginMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_mahasiswa);
+    }
+}
---
+++ app/src/main/java/com/codetalenta/eclass/RegisterMahasiswaActivity2.java
@@ -0,0 +1,14 @@
+package com.codetalenta.eclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterMahasiswaActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_mahasiswa2);
+    }
+}
