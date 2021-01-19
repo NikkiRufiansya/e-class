490d62a69013ddaede5d89381789a032c8134d14
noverio24 <noverio99@gmail.com>
2021-01-19 18:54:15 +0800

    perubahan Layout homepage

--- app/src/main/java/com/codetalenta/eclass/MainActivity.java
+++ app/src/main/java/com/codetalenta/eclass/MainActivity.java
@@ -2,14 +2,23 @@ package com.codetalenta.eclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnLogin;
+    Button btnMhs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMhs = findViewById(R.id.btnMahasiswa);
+        btnMhs.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,HomePageActivity.class);
+            startActivity(intent);
+        });
     }
 }
